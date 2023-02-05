a6701e3eac82a49618b2e4986245fff473470215
Samiul Kabir <samkabir26@gmail.com>
2023-02-06 01:01:07 +0600

    Initial Commit

---
+++ components/Header/header.js
@@ -0,0 +1,87 @@
+import { Box } from '@mui/material';
+import Link from 'next/link';
+import React from 'react';
+import { useState } from "react";
+
+const Header = () => {
+    const [navbar, setNavbar] = useState(false);
+
+    return (
+        <nav className="w-full bg-[#19263d] md:pt-0 shadow">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+            <div>
+                <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                    <a href="javascript:void(0)">
+                        <Box>
+                            <img src='/images/Logo.png' alt='Logo' className='' width="50px" />
+                        </Box>
+                    </a>
+                    <div className="md:hidden">
+                        <button
+                            className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                            onClick={() => setNavbar(!navbar)}
+                        >
+                            {navbar ? (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-6 h-6"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            ) : (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-6 h-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth={2}
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                </svg>
+                            )}
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div
+                    className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                        navbar ? "block" : "hidden"
+                    }`}
+                >
+                    <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0 font-semibold">
+                        <li className="text-gray-600 hover:text-[#7a61ff] my-2">
+                            <a href="javascript:void(0)"><span className='text-[#7a61ff]'>01.</span> About</a>
+                        </li>
+                        {/* <li className="text-gray-600 hover:text-blue-600">
+                            <a href="javascript:void(0)">Blog</a>
+                        </li> */}
+                        <li className="text-gray-600 hover:text-[#7a61ff] my-2">
+                            <a href="javascript:void(0)"><span className='text-[#7a61ff]'>02.</span> Experience</a>
+                        </li>
+                        <li className="text-gray-600 hover:text-[#7a61ff] my-2">
+                            <a href="javascript:void(0)"><span className='text-[#7a61ff]'>03.</span> Work</a>
+                        </li>
+                        <li className="text-gray-600 hover:text-[#7a61ff] my-2">
+                            <a href="javascript:void(0)"><span className='text-[#7a61ff]'>04.</span> Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    );
+};
+
+export default Header;
---
+++ components/SocialMediaLinks.js/SocialMediaLinks.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Box } from '@mui/material'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+const SocialMediaLinks = () => {
+    return (
+        <div>
+            <Box>
+                <ul className='list-none m-0 p-0 fixed overflow-auto bottom-36 left-8'>
+                    <li><a className="active" href="https://www.facebook.com/fahim.kabir.5/"><FacebookIcon className="text-4xl mb-4 hover:text-[#3b5998]" /></a></li>
+                    <li><a className="active" href="https://github.com/samkabir"><GitHubIcon className="text-4xl mb-4 hover:text-[#171515]" /></a></li>
+                    <li><a className="active" href="https://www.linkedin.com/in/samkabir/"><LinkedInIcon className="text-4xl mb-2 hover:text-[#0072b1]" /></a></li>
+                </ul>
+            </Box>
+            <Box>
+                <ul className='list-none m-0 p-0 fixed bottom-36 right-0'>
+                    <li className="rotate-90 font-semibold text-[#5845c4]" ><a href="mailto:admin@gamblingco.in">samkabir26@gmail.com</a></li>
+                </ul>
+            </Box>
+        </div>
+    );
+};
+
+export default SocialMediaLinks;
