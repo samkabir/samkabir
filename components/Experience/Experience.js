b267a2c4304defd2b200791538e3f030894ea0d4
Samiul Kabir <samkabir26@gmail.com>
2023-02-27 19:07:12 +0600

    Added the experience Section

--- components/Experience/Experience.js
+++ components/Experience/Experience.js
@@ -1,14 +1,135 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { experience } from '../../data/experience';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+
+function TabPanel(props) {
+    console.log(experience);
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
 
 const Experience = () => {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
     return (
-        <Box className='py-10'>
+        <Box className='py-10 md:h-[500px]'>
             <Box>
                 <Typography variant='h4' className='font-semibold text-[#d2d2d2]'>
                     <span className='text-[#7a61ff]'>01. </span> Job Experiences
                 </Typography>
             </Box>
+            <Box className='mt-4'>
+                <Box
+                    sx={{ flexGrow: 1 }}
+                    className='md:flex'
+                >
+                   <Box>
+                    {/* for desktop view */}
+                   <Tabs
+                        orientation="vertical"
+                        scrollButtons
+                        allowScrollButtonsMobile
+                        value={value}
+                        variant="scrollable"
+                        onChange={handleChange}
+                        aria-label="Vertical tabs example"
+                        sx={{ borderRight: 2, borderColor: 'divider' }}
+                        className='hidden md:block'
+                    >
+                        {
+                            experience && experience.map((e, i) => (
+                                <Tab label={e.job_position} key={i} className='text-white w-[180px] normal-case font-[600]' {...a11yProps(i)} />
+                            ))
+                        }
+                    </Tabs>
+                   </Box>
+                        {/* For Mobile View */}
+                    <Box sx={{ maxWidth: { xs: 320, sm: 480 } }} className='md:hidden border-2 border-black mt-2 rounded'>
+                        <Tabs
+                            value={value}
+                            onChange={handleChange}
+                            variant="scrollable"
+                            scrollButtons
+                            allowScrollButtonsMobile
+                            aria-label="scrollable force tabs example"
+                        >
+                            {
+                                experience && experience.map((e, i) => (
+                                    <Tab label={e.job_position} key={i} className='text-white w-[180px] normal-case font-[600]' />
+                                ))
+                            }
+                        </Tabs>
+                    </Box>
+
+                    {
+                        experience && experience.map((e, i) => (
+                            <TabPanel value={value} index={i}  key={i} className='md:w-[800px]' >
+                                <Box>
+                                <Box>
+                                    <Typography variant='subtitle1' className='text-white font-[600]'>
+                                        {e.company_name}
+                                    </Typography>
+                                </Box>
+                                <Box className='pb-2'>
+                                    <Typography variant='subtitle2' className='text-white font-[500] pl-4'>
+                                        {e.timeline}
+                                    </Typography>
+                                </Box>
+                                <Box>
+                                    {e.responsibilities && e.responsibilities.map((e,i) => (
+                                        <Box className='flex pt-2' key={i}>
+                                            <ArrowRightIcon  className='text-white'/>
+                                            <Typography variant='subtitle2' className='text-white' >
+                                                {e}
+                                            </Typography>
+                                        </Box>
+                                    ))}
+                                </Box>
+                                </Box>
+                            </TabPanel>
+                        ))
+                    }
+
+                </Box>
+            </Box>
+            {/* <Box className='md:hidden'>
+
+            </Box> */}
         </Box>
     );
 };
---
+++ data/experience.js
@@ -0,0 +1,26 @@
+export const experience = [
+    {
+        id:0,
+        tag:'One',
+        job_position:'Manual Tester / QA Engineer',
+        company_name:'Stealth US Digital Health Company',
+        timeline:'20th October – Present Day',
+        responsibilities:['The company uses novel technologies (like computer vision) to increase access to quality healthcare in the US. It is backed by leading tech angel investors and healthcare VCs.', 'Conducted user acceptance testing to make sure that the application meets functional and compliance needs. Found X bugs and was instrumental in final tests to prepare for product launch.', 'Conducted user acceptance testing to make sure that the application meets functional and compliance needs. Found X bugs and was instrumental in final tests to prepare for product launch.', ' Collaborated with the product team, including the CEO, and developers daily to ensure requirements were being met.', 'Documented, tracked and reproduced bugs / defects as needed as well as creating reports and steps to reproduce', 'Met all project deadlines and directly worked with CEO to provide guidance on UX and future design considerations', 'Reference call with CEO or developers available upon request.'],
+    },
+    {
+        id:1,
+        tag:'Two',
+        job_position:'Junior Software Engineer',
+        company_name:'BhaloVentures Limited - Bhalogari.com',
+        timeline:'3rd April 2022 – 2nd June 2022',
+        responsibilities:['Search AutoComplete, Bangladesh Map Search implementation, Car Compare, Dynamic Search Page (React JS) ', 'Admin DashBoard, Profile page, Edit Profile Page. (Next JS) ', 'Bug fixes.'],
+    },
+    {
+        id:2,
+        tag:'Three',
+        job_position:'Internship',
+        company_name:'BhaloVentures Limited - Bhalogari.com',
+        timeline:'3rd January 2022 – 2nd April 2022',
+        responsibilities:['Learn and be familiar the Front and Back End codebase. ', 'Minor bugfixes. - Minor CSS fixes and implementations. ', ' DRF API, EMI Calculator. ', 'Git', 'Sample React-Redux Projects (React JS).'],
+    },
+]
