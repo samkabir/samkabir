64e38f227e1cf9ae4d22a3b51194550853f02b5f
Samiul Kabir <samkabir26@gmail.com>
2025-01-13 04:14:10 +0600

    UPdated 1/13/25

--- data/contractualExperiences.js
+++ data/contractualExperiences.js
@@ -2,7 +2,7 @@ export const ContractualExperiencesData = [
     {
         id:0,
         tag:'One',
-        job_position:'Senior Frontend Engineer',
+        job_position:'Senior Frontend Engineer (Consulting project under NDA)',
         company_name:'A Food Delivery Startup',
         timeline:'July 2024 – September 2024',
         responsibilities:['Spearheaded frontend development for user, merchant, and admin dashboards.', 'Delivered critical features, including user authentication, dashboard analytics, and merchant management tools using React.js and Next.js.', 'Collaborated with cross-functional teams to design scalable architectures and ensure timely delivery of features.', 'Enhanced communication skills by working directly with stakeholders to prioritize and refine deliverables'],
@@ -10,8 +10,8 @@ export const ContractualExperiencesData = [
     {
         id:3,
         tag:'Four',
-        job_position:'Frontend Developer',
-        company_name:'European Online Casino Company',
+        job_position:'Frontend Developer (Consulting project under NDA)',
+        company_name:'Online Casino Company',
         timeline:'May 2023 – June 2024',
         responsibilities:['Designed and developed 7 blog websites with multiple routes to support SEO strategies, using React (2 projects) and Next JS (5 projects).', 'Implemented modern responsive designs, optimized for performance and search engine visibility (SSR).', 'Collaborated with clients to refine requirements, ensuring seamless integration of SEO practices with functional designs.', 'Gained expertise in building scalable, maintainable front-end architectures while managing multiple projects simultaneously.'],
     },
